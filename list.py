e332a87f035ec9ce700ab8ef924d30a64456fe23
kyuparkyu <kyuparkyu@gmail.com>
2022-05-10 23:22:20 +0900

    [day2] python list practice

---
+++ list.py
@@ -0,0 +1,13 @@
+numbers = [5,1,10,7]
+print(numbers)
+new_list = sorted(numbers, reverse=True)
+print(new_list)
+
+numbers.sort()
+print(numbers)
+
+greetings = ["안녕", "니하오", "곤니찌와", "올라", "싸와디캅", "헬로", "봉주르"]
+i = 0
+while i < len(greetings):
+    print(greetings[i])
+    i += 1
